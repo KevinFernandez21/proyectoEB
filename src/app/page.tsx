ed6d456ebefe928d9e2bc47d9c9c6d099fabcf25
Kevin Fernandes Sanchez <kdfernan@espol.edu.ec>
2024-08-15 09:28:12 -0500

    add grafh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,33 @@ export default function Home() {
     <div className="bg-black min-h-screen flex flex-col items-center justify-center text-white px-5">
       <div><h1 className="text-3xl font-bold mb-10">Ley de Kirchhoff</h1></div>
       <div className="flex">
-        <div className="relative w-1/2">
+        <div className="relative w-1/2 flex items-center justify-center">
           <Image
             src={Circuito}
             alt="Diagrama de circuito"
           />
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2 p-5">
           <p className="text-2xl font-bold mt-10 ">Ley de Kirchhoff</p>
           <p className="mt-5">La ley de Kirchhoff es una de las leyes fundamentales del análisis de circuitos eléctricos. Fue formulada por Gustav Kirchhoff en 1845. La ley de Kirchhoff se basa en la conservación de la energía y la carga eléctrica. La ley de Kirchhoff se divide en dos leyes: la ley de corrientes de Kirchhoff y la ley de tensiones de Kirchhoff.</p>
         </div>
       </div>
-      <div>
+      <div className="">
         <h1>Graficas</h1>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="flex">
+          <div className="">
+            <h1>VvsR</h1>
+            <div></div>
+          </div>
+          <div>
+            <h1>IvsR</h1>
+            <div></div>
+          </div>
+          <div>
+            <h1>PvsR</h1>
+            <div></div>
+          </div>
+        </div>
       </div>
     </div>
   );
